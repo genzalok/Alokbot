848d735c3cb85e79ca0070f0e2468d5a167d767b
♥⁠╣Sivam_uv╠⁠♥ <123348827+Botprim@users.noreply.github.com>
2023-10-02 08:05:36 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -27,7 +27,7 @@ async def start(client, message):
                     InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
                     InlineKeyboardButton('✨Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ💯', url=SPRT_CHNL),
-                    InlineKeyboardButton('💫Mᴏᴠɪᴇ Gʀᴏᴜᴘ ⚡', url=GRP_LNK)
+                    InlineKeyboardButton('🍿 REQUEST MOVIE 🍿', url=GRP_LNK)
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup, disable_web_page_preview=True)
@@ -44,13 +44,13 @@ async def start(client, message):
         buttons = [[
                     InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton('❣️Update Channel❣️', url=SPRT_CHNL),
-                    InlineKeyboardButton('⚡Mᴏᴠɪᴇ Gʀᴏᴜᴘ ⚡', url=GRP_LNK)
+                    InlineKeyboardButton('⚡️ UPDATES ⚡️', url=SPRT_CHNL),
+                    InlineKeyboardButton('🍿 REQUEST MOVIE 🍿', url=GRP_LNK)
                 ],[
-                    InlineKeyboardButton('🍿Hᴇʟᴘ🍿', callback_data='help'),
-                    InlineKeyboardButton('💲Aʙᴏᴜᴛ 💲', callback_data='about')
+                    InlineKeyboardButton('🍄 HELP 🍄', callback_data='help'),
+                    InlineKeyboardButton('🔰 ABOUT 🔰', callback_data='about')
                 ],[
-                    InlineKeyboardButton('©️Eᴀʀɴ Mᴏɴᴇʏ💲', callback_data="shortlink_info")
+                    InlineKeyboardButton('💰 EARE MONEY WITH BOT 💸', callback_data="shortlink_info")
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)   
         m=await message.reply_sticker("CAACAgQAAxkBAAECtXhktTlDe2ex0KXhgS33tJUa3xRsdgACjgoAAl90AVKfNpKbcyBQNB4E") 
@@ -97,13 +97,13 @@ async def start(client, message):
         buttons = [[
                     InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton('🍿Update Channel🍿', url=SPRT_CHNL),
-                    InlineKeyboardButton('🎥 Mᴏᴠɪᴇ Gʀᴏᴜᴘ🎥', url=GRP_LNK)
+                    InlineKeyboardButton('⚡️ UPDATES ⚡️', url=SPRT_CHNL),
+                    InlineKeyboardButton('🍿 REQUEST MOVIE 🍿', url=GRP_LNK)
                 ],[
-                    InlineKeyboardButton('↼ Hᴇʟᴘ', callback_data='help'),
-                    InlineKeyboardButton('⇀ Aʙᴏᴜᴛ', callback_data='about')
+                    InlineKeyboardButton('🍄 HELP 🍄', callback_data='help'),
+                    InlineKeyboardButton('🔰 ABOUT 🔰', callback_data='about')
                 ],[
-                    InlineKeyboardButton('💲Eᴀʀɴ Mᴏɴᴇʏ💲', callback_data="shortlink_info")
+                    InlineKeyboardButton('💰 EARE MONEY WITH BOT 💸', callback_data="shortlink_info")
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)      
         await message.reply_photo(
