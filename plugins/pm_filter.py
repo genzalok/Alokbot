8e74c82778ff0558a9384b8143a79b6da5e6a4c2
♥⁠╣Sivam_uv╠⁠♥ <123348827+Botprim@users.noreply.github.com>
2023-10-07 11:56:01 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2142,7 +2142,7 @@ async def auto_filter(client, msg, spoll=False):
     if imdb and imdb.get('poster'):
         try:
             hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
-            await dlt.delete()
+            await m.delete()
             try:
                 if settings['auto_delete']:
                     await asyncio.sleep(300)
