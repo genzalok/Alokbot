b942cb68dd56cb2c52982e64138804ed1459e86d
♥⁠╣Sivam_uv╠⁠♥ <123348827+Botprim@users.noreply.github.com>
2023-10-07 13:48:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2186,7 +2186,7 @@ async def auto_filter(client, msg, spoll=False):
                 await message.delete()
     else:
         fuk = await message.reply_text(text=cap, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True)
-        await dlt.delete()
+        await m.delete()
         try:
             if settings['auto_delete']:
                 await asyncio.sleep(300)
